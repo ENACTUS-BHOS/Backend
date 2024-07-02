2893bf686c4d0481474814ca1b795572ca3fe1aa
Rana-Isayeva <153900683+Rana-Isayeva@users.noreply.github.com>
2024-07-02 13:36:02 +0400

    Delete Backend directory

--- Backend/Models/Exhibition.cs
+++
@@ -1,8 +0,0 @@
-public class Exhibition
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string PictureUrl { get; set; }
-    public string VideoUrl { get; set; }
-}
