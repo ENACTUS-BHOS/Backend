76d6f4b5aeed0038eb29a5f72db074e693b969fa
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-09-13 16:15:29 +0400

    Update cors policy

--- src/Backend.Presentation/Program.cs
+++ src/Backend.Presentation/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("BlazorWasmPolicy", corsBuilder =>
     {
         corsBuilder
-            .WithOrigins("https://artlegacy.co", "https://www.artlegacy.co")
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
